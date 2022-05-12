3df4975d17f068cf465ad39571dd3d3bd23a10dd
Diegoq28 <quevedod67@gmail.com>
2022-05-12 18:09:32 -0500

    esto ni se pa dinde va

---
+++ Controller/controller.js
@@ -0,0 +1,14 @@
+
+/*--------------------->>>REQUERIMIENTO DE PAQUETES<<<--------------------*/
+
+    const connection=require('../Connection/conexion');
+    const cnn=connection();
+    const {render}=require('ejs');
+    const { query } = require('express');
+    const controller={};
+
+/*---------------------------------------------------------------*/
+
+/*-------------------------------->>><<<-------------------------------*/
+//separador de bloques de codigo
+/*---------------------------------------------------------------*/
---
+++ index.js
@@ -0,0 +1,47 @@
+
+/*-------------->>>Requerimiento de paquetes<<<-----------------------*/
+    const express=require('express');
+    const morgan =require('morgan');
+    const path=require('path');
+    const session=require('express-session');
+
+    const app=express();
+
+/*---------------------------------------------------------------*/
+
+/*----------------->>>Generacon de funciones<<<--------------------*/
+
+    app.use(morgan('dev'));
+    
+    app.engine('html',require('ejs').renderFile);
+    app.use(express.static(path.join(__dirname,'Public')));
+    
+    app.set('view engine','ejs');
+    app.set('views',path.join(__dirname,'Views'));
+
+    app.use(session({
+        secret: '12345',
+        resave: false,
+        saveUninitialized: true
+    }));
+    
+    app.use(express.urlencoded({extended:true}));
+    app.use(require('./Routes/rutas'));
+    app.use((err,res,req,next)=>{
+        res.send({err:err.message});
+    })
+
+/*---------------------------------------------------------------*/
+
+/*------------------->>>Configuracion del servidor<<<------------------*/
+
+    app.set('port',process.env.PORT || 3000);
+    app.listen(app.get('port'),()=>{
+        console.log(`Server on port  ${app.get('port')}`)
+    });
+
+/*---------------------------------------------------------------*/
+
+/*-------------------------------->>><<<-------------------------------*/
+//separador de bloques de codigo
+/*---------------------------------------------------------------*/
