37144bf6fcad8be52f627e64f755871bdf530ff2
Diegoq28 <quevedod67@gmail.com>
2022-05-18 01:51:03 -0500

    inplementacion de la primera vista y diseño base

--- Controller/controller.js
+++ Controller/controller.js
@@ -5,9 +5,24 @@
     const cnn=connection();
     const {render}=require('ejs');
     const { query } = require('express');
+    const res = require('express/lib/response');
     const controller={};
 
 /*---------------------------------------------------------------*/
+
+/*------------------>>>controlador del home<<<---------------------*/
+
+controller.casa=(req,res,next)=>{
+    res.render('home');
+}
+
+/*---------------------------------------------------------------*/
+
+/*------------------>>>exportar controlador<<<-------------------*/
+module.exports=controller;
+/*---------------------------------------------------------------*/
+
+
 
 /*-------------------------------->>><<<-------------------------------*/
 //separador de bloques de codigo
