03f4915b7e97cbff522f24e15e0cacc71fe1d221
Diegoq28 <quevedod67@gmail.com>
2022-05-18 14:31:39 -0500

    ingreso de plantillas nuevas para menus en ejs

--- Controller/controller.js
+++ Controller/controller.js
@@ -13,8 +13,32 @@
 /*------------------>>>controlador del home<<<---------------------*/
 
 controller.casa=(req,res,next)=>{
-    res.render('home');
+    res.render('registro_asociado');
 }
+
+controller.login=async(req,res,next)=>{
+    res.render('login');
+    //const usu=await req.body[0];
+    //const cla=await req.body[1];
+    //console.log(usu+cla);
+    /* cnn.query('SELECT * FROM tl_usuarios  WHERE correo=? AND password=?',[usu,cla],(err,results)=>{
+        if(err){
+            next(new Error("error de consulta login",err));
+            
+        }
+        else if(results!=0){
+            uss= results[0].nombre_usuario;
+            console.log(".."+uss);
+            req.session.login=true;
+            res.redirect('/');
+                }
+        else{
+            //console.log("datos Incorrectos");
+            res.redirect('/');
+        }
+    }); */
+}
+
 
 /*---------------------------------------------------------------*/
 
